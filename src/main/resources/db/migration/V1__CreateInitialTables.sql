2cb8d987a25aee2423dd18f24828d5c7292e4948
Nikolova,Rositsa R.P <r.nikolova@student.fontys.nl>
2023-01-12 14:48:03 +0000

    Updating flyway with correct formatting V1__CreateInitialTables.sql

--- src/main/resources/db/migration/V1__CreateInitialTables.sql
+++ src/main/resources/db/migration/V1__CreateInitialTables.sql
@@ -1,43 +1,43 @@
 CREATE TABLE users
 (
-    id int NOT NULL AUTO_INCREMENT,
-    role enum('ADMIN','HOMEOWNER','RENTER') NOT NULL,
-    email varchar(45)  NOT NULL,
-    password varchar(200) NOT NULL,
-    firstName varchar(45)  NOT NULL,
-    lastName varchar(45)  NOT NULL,
+    id        int                                NOT NULL AUTO_INCREMENT,
+    role      enum('ADMIN','HOMEOWNER','RENTER') NOT NULL,
+    email     varchar(45)                        NOT NULL,
+    password  varchar(200)                       NOT NULL,
+    firstName varchar(45)                        NOT NULL,
+    lastName  varchar(45)                        NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE (email,email)
+    UNIQUE(email)
 );
 
 CREATE TABLE properties
 (
-    id int NOT NULL AUTO_INCREMENT,
-    price double NOT NULL,
-    propertyType enum('APARTMENT','HOUSE','ROOM','STUDIO') NOT NULL,
-    city varchar(45) NOT NULL,
-    street varchar(45) NOT NULL,
-    streetNumber int NOT NULL,
-    postcode varchar(45) NOT NULL,
-    livingSpace int NOT NULL,
-    numberOfRooms int NOT NULL,
-    numberOfBedrooms int NOT NULL,
-    propertyStatus enum('ACTIVE','INACTIVE') NOT NULL,
-    ownerId int DEFAULT NULL,
+    id                 int                                       NOT NULL    AUTO_INCREMENT,
+    price              double                                    NOT NULL,
+    propertyType       enum('APARTMENT','HOUSE','ROOM','STUDIO') NOT NULL,
+    city               varchar(45)                               NOT NULL,
+    street             varchar(45)                               NOT NULL,
+    streetNumber       int                                       NOT NULL,
+    postcode           varchar(45)                               NOT NULL,
+    livingSpace        int                                       NOT NULL,
+    numberOfRooms      int                                       NOT NULL,
+    numberOfBedrooms   int                                       NOT NULL,
+    propertyStatus     enum('ACTIVE','INACTIVE')                 NOT NULL,
+    ownerId            int                                       DEFAULT NULL,
     PRIMARY KEY (id),
     UNIQUE (id, ownerId),
-    FOREIGN KEY (ownerId) REFERENCES users(id),
+    FOREIGN KEY (ownerId) REFERENCES users(id)
 );
 
 CREATE TABLE requests
 (
-    idRequest int NOT NULL AUTO_INCREMENT,
-    idProperty int NOT NULL,
-    idHomeowner int NOT NULL,
-    status enum('PENDING','ACCEPTED','REJECTED') NOT NULL,
-    date datetime NOT NULL,
-    resonRejection varchar(100) DEFAULT NULL,
+    idRequest      int                                    NOT NULL AUTO_INCREMENT,
+    idProperty     int                                    NOT NULL,
+    idHomeowner    int NOT NULL,
+    status         enum('PENDING','ACCEPTED','REJECTED')  NOT NULL,
+    date           datetime                               NOT NULL,
+    resonRejection varchar(100)                           DEFAULT NULL,
     PRIMARY KEY (idRequest),
     FOREIGN KEY (idHomeowner) REFERENCES users (id),
     FOREIGN KEY (idProperty) REFERENCES properties (id)
-)
+);
